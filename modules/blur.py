0eedebcff30eb7a4a503f47155657a5c08059675
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-02-16 09:58:18 +0200

    Change cleaning kwargs

--- modules/blur.py
+++ modules/blur.py
@@ -15,7 +15,6 @@ def gaus(**kwargs):
 
   kernel = kwargs.get('k', 3)
   kX = kY = kernel 
-  kwargs.pop('k', None)
 
   kwargs['image'] = cv2.GaussianBlur(kwargs['image'], (kX, kY), 0)
 
@@ -25,7 +24,6 @@ def gaus(**kwargs):
 def median(**kwargs):
 
   kernel = kwargs.get('k', 3)
-  kwargs.pop('k', None)
 
   kwargs['image'] = cv2.medianBlur(kwargs['image'], kernel)
 
