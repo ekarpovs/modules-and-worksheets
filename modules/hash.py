72a3bd4c2260d3b5cff06cfbfc095e080740ff1a
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-02-27 15:06:09 +0200

    Fix dhash

--- modules/hash.py
+++ modules/hash.py
@@ -1,13 +1,14 @@
 import cv2
 # Hashing operations
 
-def dhash(**kwargs):
+def dhashm(**kwargs):
 
   # compute the (relative) horizontal gradient between adjacent
   # column pixels
-  diff = kwargs['dhash'][:, 1:] > kwargs['dhash'][:, :-1]
+  diff = kwargs['image'][:, 1:] > kwargs['image'][:, :-1]
 	# convert the difference image to a hash
   dhash = sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
   kwargs['dhash'] = dhash
 
   return kwargs
