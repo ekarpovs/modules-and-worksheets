00e1f0994cb6614d340b156c4d42fb1e4966d02e
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-03-11 12:25:21 +0200

    cmp module - add cmp_psnr and diff operations

--- modules/cmp.py
+++ modules/cmp.py
@@ -3,9 +3,11 @@ Compare operations
 '''
 import cv2
 import numpy as np
+from skimage.measure import compare_ssim
 # from skimage.measure import structural_similarity as ssim
+from math import log10, sqrt
 
-def cpm_mse(**kwargs):
+def cmp_mse(**kwargs):
   '''
   Calculates 'Mean Squared Error' between pixels of two images.
   The 'Mean Squared Error' between the two images is the
@@ -14,7 +16,7 @@ def cpm_mse(**kwargs):
 
   Keyword arguments (key, default):
   - image: an original image;
-  - image: an image to compare;
+  - image1: an image to compare;
 
   Returns:
   - images;
@@ -26,6 +28,8 @@ def cpm_mse(**kwargs):
 
   kwargs['mse'] = err
 
+  print("mse error", err)
+
   return kwargs
 
 
@@ -33,17 +37,83 @@ def cmp_ssim(**kwargs):
   '''
   Calculates 'Structural Similarity Index' between pixels of two images.
   Uses to compare two windows instead an entire images.
+  This two images must have the same dimension.
 
   Keyword arguments (key, default):
   - image: an original image;
-  - image: an image to compare;
+  - image1: an image to compare;
 
   Returns:
   - images;
   - ssim: Structural Similarity Index.
   '''  
 
-  # s = ssim(kwargs['image'], kwargs['image1'])
-  # kwargs['ssim'] = None 
+  s, diff = compare_ssim(kwargs['image'], kwargs['image1'], full=True, multichannel=True)
+  kwargs['ssim'] = s 
+
+  print("ssim", s)
 
   return kwargs
+
+
+def cmp_psnr(**kwargs):
+  '''
+  Calculates 'Peak Signal-to-Noise Ratio' between two images.
+  This two images must have the same dimension.
+
+  Keyword arguments (key, default):
+  - image: an original image;
+  - image1: an image to compare;
+
+  Returns:
+  - images;
+  - psnr: Peak Signal-to-Noise Ratio.
+  '''  
+
+  mse = np.mean((kwargs['image'] - kwargs['image1']) ** 2)
+  psnr = 100
+  if(mse != 0):
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+
+  kwargs['psnr'] = psnr 
+
+  print("psnr", psnr)
+
+  return kwargs
+
+
+def cmp_norm(**kwargs):
+  '''
+  Calculates 'Pixels difference' between two GRAY images.
+  This two images must have the same dimension.
+
+  Keyword arguments (key, default):
+  - image: an original image;
+  - image1: an image to compare;
+
+  Returns:
+  - images;
+  - psnr: Difference.
+  '''  
+  image = kwargs['image']
+  image1 = kwargs['image1']
+
+  norm = image/np.sqrt(np.sum(image**2))  
+  norm1 = image1/np.sqrt(np.sum(image1**2))  
+  diff =np.sum(norm*norm1)
+  
+  kwargs['diff'] = diff 
+
+  print("diff", diff)
+
+  # https://stackoverflow.com/questions/11541154/checking-images-for-similarity-with-opencv
+  # picture1 = np.random.rand(100,100)
+  # picture2 = np.random.rand(100,100)
+  # picture1_norm = picture1/np.sqrt(np.sum(picture1**2))
+  # picture2_norm = picture2/np.sqrt(np.sum(picture2**2))
+  # print(np.sum(picture1_norm*picture2_norm))
+
+  return kwargs
+
+
