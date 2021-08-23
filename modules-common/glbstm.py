bda01cb5ed2a90f1446f7a915668b192c079b659
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-08-23 10:30:53 +0300

    Add while global statement

--- modules-common/glbstm.py
+++ modules-common/glbstm.py
@@ -1,19 +1,36 @@
 '''
-Local module, contains global staments implementation
+Local module, contains global staments descriptions
 '''
 
-def forinrange(step, **kwargs):
+def forinrangestm(step, **kwargs):
   '''
   Presents the statement parameters.
 
   Keyword arguments:
 
   Step arguments:
+  --n;s;[];"0"-- include: number of flow steps are in the loop
   --n;s;[];"0"-- start: the first value in the range
   --n;s;[];"0"-- stop:  the upper bound of the range
   --n;s;[];"0"-- step:  the value controls the increment between the values in the range
   --s;s;[];""-- i: next step parameter name that will get i value
+
+  Returns:
+  - kwargs as is
+  '''
+  return kwargs
+
+
+def whilestm(step, **kwargs):
+  '''
+  Presents the statement parameters.
+
+  Keyword arguments:
+
+  Step arguments:
   --n;s;[];"0"-- include: number of flow steps are in the loop
+  --s;s;[];"true"-- cond: condition 
+  --s;s;[];""-- m: map condition to a predicate inside while stm
 
   Returns:
   - kwargs as is
