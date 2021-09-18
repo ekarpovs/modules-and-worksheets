c23e0a26d6412ca2b4c97de15124170b91b768aa
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-09-18 12:21:29 +0300

    Improve the module doc

--- modules-common/common.py
+++ modules-common/common.py
@@ -1,16 +1,7 @@
 '''
-Local module, contains common utility operations
-'''
-import cv2
+Contains common utility operations
 
-def empty(step, **kwargs):
-  '''
-  It is an operation's implementation boilerplate.
-
-  @kwargs:
-  - image: an image that will be processed
-
-  Step arguments pattern:
+  A setup parameter pattern:
   --Type;Domain;[Possible Values];Default-- name: description
 
   Where:
@@ -23,89 +14,108 @@ def empty(step, **kwargs):
     Domain                    Possible values in the domain
       s (single value)          V
       l (list of values)        V1,V2,V3...,Vn
-      d (dict of key/values)    N1:V1,N2:V2,N3:V3...,Nn:Vn
+      d (dict of key/value)    K1:V1,K2:V2,K3:V3...,Kn:Vn
       r (range,step)            Nmin,Nmax,Step
       f (flag)                  0,1 or True,False
+'''
+import cv2
 
-  Returns:
-  - the kwargs as is.
+def empty(params, **data):
+  '''
+  It is an operation's implementation boilerplate.
+
+  parameters:
+    - params: - dictionary of the operation setup parameters 
+    - data: - dictionary of references to a data that will be processed
+  returns:
+    - data: - dictionary of references to a data that have been processed
   '''  
-
-  print("step", step)
-  print("kwargs")
-  [print(k, v) for k, v in kwargs.items() if k != "image" and k != 'orig']
-
-  return kwargs
+  return data
 
 
-def store(step, **kwargs):
+def store(params, **data):
   '''
   Stores an image into a file.
   
-  @kwargs:
-  - image: an image that will be stored;
- 
-  @step:
-  --str;s[];""-- ffn: full file name, where the image will be stored
-  
-  Returns:
-  - the kwargs as is.
-  '''  
-  ffn = step.get('ffn', '')
+  parameters:
+    - params: 
+      --str;s[];""-- ffn: full file name, where the image will be stored
 
-  image = kwargs.get('image')
+    - data: - reference to an image that will be stored
+
+  returns:
+    - data: 
+      - reference to the stored image
+  '''  
+  ffn = params.get('ffn', '')
+  image = params.get('image')
+
   if image is not None:
     cv2.imwrite(ffn, image)
-  return kwargs
+  return data
 
 
-def restore(step, **kwargs):
+def restore(params, **data):
   '''
   Restores an image from a file.
   
-  @step:
-  --str;s;[];""--  ffn: full file name, where from the image will be restored.
-  --str;s;[];""-- index: suffix for new 'image' key - 'image+idx'
+  parameters:
+    - params: 
+      --str;s;[];""--  ffn: full file name, where from the image will be restored.
+      --str;s;[];"image"-- key:  name of a reference to the restored image
   
-  Returns:
-  - the image.
+  returns:
+    - data: 
+      - reference to the restored image
   '''  
-  ffn = step.get('ffn', '')
-  index = step.get('index', '')
+  ffn = params.get('ffn', '')
+  key = params.get('key', 'image')
 
-  key = 'image' + index 
-  print(key, ffn)
   if ffn != '':
-    kwargs[key] = cv2.imread(ffn)
+    data[key] = cv2.imread(ffn)
   else:
-    kwargs[key] = kwargs['image']
-  return kwargs
+    data[key] = data['image']
+  return data
 
 
-def clean_kwargs(step, **kwargs):
+def clean_data(params, **data):
   '''
-  Cleans kwargs dictionary from defined items.
+  Cleans data dictionary from defined items.
   
-  @step:
-  --str;s;[];""-- keys: keys, separated by ';' , that will be removed from the kwargs
+  parameters:
+    - params: 
+      --str;s;[];""-- keys: keys, separated by ',' , that will be removed from the data
 
-  Returns:
-  - the kwargs without removed items
+    - data: - dictionary of references to a data that will be processed
+
+  returns:
+  - data without removed items
   '''  
-  keys = step.get('keys')
+  keys = params.get('keys')
+  keys_list = keys.split(',')
+  keys_list = [k.strip() for k in keys_list]
+  if len(keys_list) > 0:
+    keys_list = keys.split(',')
+    for key in [k for k in data if k in keys_list]: data.pop(key, None)
+  return data
 
-  if keys is not None:
-    for key in [k for k in kwargs if k in keys]: kwargs.pop(key, None)
-  return kwargs
 
-
-def print_kwargs(step, **kwargs):
+def print_data(params, **data):
   '''
-  Prints kwargs.
+  Prints data.
 
-  Returns:
-  - the kwargs as is.
+  parameters:
+    - params: 
+      --str;s;[];image, orig-- keys: keys, separated by ',' , that will be not printed
+
+    - data: - dictionary of references to a data that will be processed
+
+  returns:
+    - the data as is
   '''
-  [print(k, v) for k, v in kwargs.items() if k != "image" and k != "orig"] 
-  return kwargs
+  keys = params.get('keys', 'image, orig')
+  keys_list = keys.split(',')
+  keys_list = [k.strip() for k in keys_list]
+  [print(k, v) for k, v in data.items() if k not in keys_list] 
+  return data
 
