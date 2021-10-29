6ebd9a6c08a015f08f3516c4ff49f808e3097ee3
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-10-29 11:40:26 +0300

    Add begin/emd implementation

--- modules-common/glbstm.py
+++ modules-common/glbstm.py
@@ -2,35 +2,33 @@
 Local module, contains global staments descriptions
 '''
 
-def forinrangestm(params, **data):
-  '''
-  Presents the statement parameters.
+from typing import Dict
 
-  parameters:
-    - params: 
-      --n;s;[];0-- include: number of flow steps are in the loop
-      --n;s;[];0-- start: the first value in the range
-      --n;s;[];0-- stop:  the upper bound of the range
-      --n;s;[];0-- step:  the value controls the increment between the values in the range
-      --s;s;[];""-- map: parameter name that will get current value
-    - data: 
-  returns:
-    - data: as is
+
+def begin(params: Dict , **data: Dict) -> Dict:
   '''
+  The first in a flow.
+
+  Parameters:
+    - params keys:   
+    - data keys: 
+  Returns:
+    - data keys:
+  '''
+
   return data
 
 
-def whilestm(params, **data):
+def end(params: Dict , **data: Dict) -> Dict:
   '''
-  Presents the statement parameters.
+  The last in a flow.
 
-  parameters:
-    - params: 
-      --n;s;[];0-- include: number of flow steps are in the loop
-      --s;s;[];"true"-- cond: condition 
-      --s;s;[];""-- map: map condition to a predicate inside while stm
-    - data: 
-  returns:
-    - data: as is
+  Parameters:
+    - params keys:   
+    - data keys: 
+  Returns:
+    - data keys:
   '''
+
   return data
+
