8cf6e384619a1b66f36cf456a3e8f65fda4bad53
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-12-05 08:48:12 +0200

    change rotation defenition

--- modules/bsc.py
+++ modules/bsc.py
@@ -179,7 +179,7 @@ def rotate_inside(params: Dict , **data: Dict) -> Dict:
   
   Parameters:
     - params:   
-      angle: float=0.0; rotation angle
+      angle:List[float](0.0,0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0)=0.0; rotation angle
       neg: bool=True; negative direction
     - data: 
       image: np.dtype; the image
