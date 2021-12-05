9469385fca516966b89a64fa553bf5b27bd5c308
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-12-05 08:09:23 +0200

    Add bilaterial filter

--- modules/blur.py
+++ modules/blur.py
@@ -61,3 +61,28 @@ def median(params: Dict , **data: Dict) -> Dict:
   kernel = params.get('kernel', 3)
   data['image'] = cv2.medianBlur(data.get('image'), kernel) 
   return data
+
+def bilateral(params: Dict , **data: Dict) -> Dict:
+  '''
+  Bilateral bluring.
+
+  Parameters:
+    - params:   
+      d: List[int](7,9,11,15,17,19,21,23,25,27)=11; diameter of each pixel neighborhood
+      sigmacolor: int=17; filter sigma in the color space.
+      sigmaspace: int=17; filter sigma in the coordinate space.
+      border: Dict[str,int](DEFAULT:4,REFLECT:2,REFLECT101:4,REFLECT_101:4,REPLICATE:1,TRANSPARENT:5,WRAP:3)=DEFAULT; pixel extrapolation method cv2.BORDER_(...)
+    - data: 
+      image: np.dtype; the image
+  Returns:
+    - data:
+      image: np.dtype; the blured image
+  '''
+
+  d = params.get('d', 11)
+  sigma_color = params.get('sigmacolor', 17)
+  sigma_space = params.get('sigmaspace', 17)
+  border_type = params.get('border',cv2.BORDER_DEFAULT) # Pixel extrapolation method
+
+  data['image'] = cv2.bilateralFilter(data.get('image'), d, sigma_color, sigma_space, border_type) 
+  return data
