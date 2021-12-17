7a78af3a97822be3472445d1b23fb9d00be17089
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-12-17 12:23:12 +0200

    Add within_bound oper

--- modules/edge.py
+++ modules/edge.py
@@ -28,32 +28,6 @@ def canny(params: Dict , **data: Dict) -> Dict:
   data['image'] = cv2.Canny(data.get('image'), threshold1, threshold2)
   return data
 
-def sobel(params: Dict , **data: Dict) -> Dict:
-  '''
-  Computes gradients along the X or Y axis uses Sobel algorithm.
-
-  Parameters:
-    - params:   
-      direction: Dict[str,int](horizontal:0,vertical:1)=horizontal; direction (x, y)
-    - data: 
-      image: np.dtype; the image
-  Returns:
-    - data:
-      image: np.dtype; the result image
-  '''
-  direction = params.get('direction', 0)
-  dx = 1
-  dy = 0
-  if direction == 1:
-    dx = 0
-    dy = 1
-  # compute gradients along the X or Y axis
-  g = cv2.Sobel(data.get('image'), ddepth=cv2.CV_64F, dx=dx, dy=dy) 
-  # images are now of the floating point data type,
-  # so convert them back a to unsigned 8-bit integer representation
-  data['image'] = cv2.convertScaleAbs(g)
-  return data
-
 def laplacian(params: Dict , **data: Dict) -> Dict:
   '''
   Computes the Laplacian of the image .
@@ -69,6 +43,78 @@ def laplacian(params: Dict , **data: Dict) -> Dict:
   lap = cv2.Laplacian(data.get('image'), cv2.CV_64F)
   data['image'] = np.uint8(np.absolute(lap)) 
   return data
+
+def sobel(params: Dict , **data: Dict) -> Dict:
+  '''
+  Computes gradients along the X or Y axis uses Sobel algorithm.
+
+  Parameters:
+    - params:   
+      direction: Dict[str,int](horizontal:0,vertical:1)=horizontal; direction (x, y)
+      convert: bool=True; convert result to unsigned 8-bit integer representation
+    - data: 
+      image: np.dtype; the image
+  Returns:
+    - data:
+      image: np.dtype; the result image
+  '''
+
+  convert = params.get('convert', True)
+  direction = params.get('direction', 0)
+  
+  dx = 1
+  dy = 0
+  if direction == 1:
+    dx = 0
+    dy = 1
+  # compute gradients along the X or Y axis
+  grad = cv2.Sobel(data.get('image'), ddepth=cv2.CV_64F, dx=dx, dy=dy) 
+  # images are now of the floating point data type,
+  # so convert them back a to unsigned 8-bit integer representation
+  if convert:
+    grad = cv2.convertScaleAbs(grad)
+  data['image'] = grad
+  return data
+
+def within_bound(params: Dict , **data: Dict) -> Dict:
+  '''
+  Computes the gradient magnitude and orientation.
+  Find all pixels that are within the upper and low angle boundaries
+
+  Parameters:
+    - params: 
+      lower: Scale[float](150.0,180.0,1.0, 0)=175.0; lower orientation angle
+      upper: Scale[float](150.0,180.0,1.0, 0)=180.0; upper orientation angle
+    - data:
+      gray: np.dtype; gray image, that was passed to Sobel
+      image-gx: np.dtype; the first image
+      image-gy: np.dtype; the second image
+  Returns:
+    - data:
+      mask: np.dtype; pixels that are within
+  '''
+
+  lower = params.get('lower', 0)
+  upper = params.get('upper', 0)
+
+  gray = data.get('gray') 
+  image_gx = data.get('image-gx') 
+  image_gy = data.get('image-gy') 
+
+  # compute the gradient magnitude and orientation, respectively
+  mag = np.sqrt((image_gx ** 2) + (image_gy ** 2))
+  orientation = np.arctan2(image_gy, image_gx) * (180 / np.pi) % 180
+  # find all pixels that are within the upper and low angle boundaries
+  idxs = np.where(orientation >= lower, orientation, -1)
+  idxs = np.where(orientation <= upper, idxs, -1)
+  mask = np.zeros(gray.shape, dtype="uint8")
+  mask[idxs > -1] = 255 
+
+  # data['mag'] = mag
+  data['mask'] = mask
+  return data
+
+
 
 #scharr
 #norm_factor = 32
