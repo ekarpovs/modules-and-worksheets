622cb13b0307737cf796667bedf3a63a440c9d77
Evgeny Karpovsky <ekarpovsky@gmail.com>
2022-03-22 10:01:34 +0200

    Chamge 2d filter - use separate channels

--- modules/filters.py
+++ modules/filters.py
@@ -14,11 +14,9 @@ def gabor_kernel(params: Dict , **data: Dict) -> Dict:
   Parameters:
     - params:   
       ksize: Scale[int](3,128,1,0)=3; size of the filter returned.
-      sigma: Scale[int](0,10,1,0)=4; standard deviation of the gaussian envelope.
-      small: bool=False; use small scaled theta
+      sigma: Scale[int](0,10,1,0)=3; standard deviation of the gaussian envelope.
       theta: Scale[int](0,90,1,0)=0; orientation of the normal to the parallel stripes of a Gabor function.
-      stheta: Scale[float](0.0,5.0,0.1,0.0)=0.0; small size orientation of the normal to the parallel stripes of a Gabor function.
-      lambd: Scale[int](5,25,5,0)=5; Wavelength of the sinusoidal factor.
+      lambd: Scale[int](5,25,1,0)=5; Wavelength of the sinusoidal factor.
       psi:  Scale[float](0.0,5.0,0.1,0.0)=0.0; phase offset.
       gamma: Scale[int](0,100,10,0)=0; spatial aspect ratio
       ktype: Dict[str,int](CV_32F:5,CV_64F:6)=CV_32F; type of filter coefficients. It can be CV_32F or CV_64F .
@@ -29,7 +27,7 @@ def gabor_kernel(params: Dict , **data: Dict) -> Dict:
   '''
 
   ksize = params.get('ksize', 3)
-  sigma = params.get('sigma', 4)
+  sigma = params.get('sigma', 3)
   small = params.get('small', False)
   stheta = params.get('stheta', 0.0)
   theta = params.get('theta', 0)
@@ -38,8 +36,6 @@ def gabor_kernel(params: Dict , **data: Dict) -> Dict:
   gamma = params.get('gamma', 0)
   ktype = params.get('ktype', cv2.CV_32F)
 
-  if small:
-    theta = stheta
   kernel = cv2.getGaborKernel((ksize, ksize), sigma, np.deg2rad(theta), lambd, psi, gamma, ktype)
 
   # f = np.fft.fft2(kernel)
@@ -67,13 +63,21 @@ def filter_2d(params: Dict , **data: Dict) -> Dict:
       image: ndarray; the result image
   '''
 
-  ddepth = params.get('ddepth', cv2.CV_32F)
+  ddepth = params.get('ddepth', -1)
   
   image= data.get('image')
   kernel= data.get('kernel')
+  shape_len = len(image.shape)
+  if shape_len >= 3:
+    layer_blue = cv2.filter2D(src=image[:,:,0], ddepth=ddepth, kernel=kernel)
+    layer_green = cv2.filter2D(src=image[:,:,1], ddepth=ddepth, kernel=kernel)
+    layer_red = cv2.filter2D(src=image[:,:,2], ddepth=ddepth, kernel=kernel)    
+      
+    new_image = np.zeros(list(layer_blue.shape) + [3], dtype='uint8')
+    new_image[:,:,0], new_image[:,:,1], new_image[:,:,2] = layer_blue, layer_green, layer_red
+  else:
+    new_image = cv2.filter2D(src=image, ddepth=ddepth, kernel=kernel)
 
-  kernel = cv2.filter2D(image, ddepth, kernel)
-
-  data['kernel'] = kernel
+  data['image'] = new_image
   return data
 
